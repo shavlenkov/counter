b8a755f101bf065b9e6659ba9a0af41383c22bd3
Pavel Shavlenkov <pashashavlenkov@gmail.com>
2023-03-15 18:26:51 +0200

    init

---
+++ src/components/Counter.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="counter">
+    <button @click="minus" class="minus">-</button>
+    <input :value="counter" type="number">
+    <button @click="plus" class="plus">+</button>
+
+    <br/>
+    <p>Next: </p>{{next + 1}} <br/>
+    <p>Prev: </p>{{prev}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Counter",
+  data() {
+    return {
+      counter: 0,
+      next: 0,
+      prev: 0
+    }
+  },
+
+  methods: {
+    plus() {
+      this.counter++;
+    },
+
+    minus() {
+      this.counter--;
+    }
+  },
+
+  watch: {
+    counter(newValue, oldValue) {
+      this.prev = oldValue;
+      this.next = newValue;
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
